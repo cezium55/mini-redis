40695c3a14f9e5e002d4b321f3ee9b6b12bb49dd
Garvit Gaur <garvitgaur2006@gmail.com>
2026-07-06 20:18:34 +0530

    Initial commit: basic socket boilerplate for mini-redis

---
+++ main.py
@@ -0,0 +1,46 @@
+from gevent import socket
+from gevent.pool import Pool
+from gevent.server import StreamServer
+
+from collections import namedtuple
+from io import BytesIO
+from socket import error as socket_error
+
+class CommandError(Exception) : pass
+class Disconnect(Exception) : pass
+
+Error = namedtuple('Error', ('message',))
+
+class protocol_handler(object):
+    def handle_request(self, socket_file) : pass
+    def write_request(self, socket_file, data) : pass
+
+class Server(object):
+    def __init__(self, host='127.0.0.1', port = 0, max_clients=64):
+        self._pool = Pool(max_clients)
+        self._server = StreamServer((host,port), self.connection_handler, spawn=self._pool)
+        
+        self._protocol = protocol_handler()
+        self._kv={}
+    
+    def connection_handler(self, conn, address):
+        socket_file = conn.makefile('rwb')
+
+        while True:
+            try:
+                data = self._protocol.handle_request(socket_file)
+            except Disconnect:
+                break
+            
+            try:
+                resp=self.get_response(data)
+            except CommandError as exc:
+                resp = Error(exc.args[0])
+            self._protocol.write_response(socket_file,resp)
+
+    def get_response(self, data):
+
+        pass
+
+    def run(self):
+        self._server.serve_forever()
